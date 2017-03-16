30de336f4fe5a3d1e3d35da61faf842aa449dbee
Jesse <jessehintz44@gmail.com>
2017-03-16 15:34:32 -0600

    updated website with home and about pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  root 'pages#home'
+  # above line is shortcut for: get '/' => 'pages#home'
+  get 'about' => 'pages#about'
+  get 'contact-us' => 'pages#contact-us'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
