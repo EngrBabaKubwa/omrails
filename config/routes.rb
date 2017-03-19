2a4886519b8e85deea9627a1f22a03b70099b1ee
Jesse <jessehintz44@gmail.com>
2017-03-19 17:34:10 -0600

    added contact us page and filled in some information for the first assignment

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root 'pages#home'
   # above line is shortcut for: get '/' => 'pages#home'
   get 'about' => 'pages#about'
-  get 'contact-us' => 'pages#contact-us'
+  get 'contact' => 'pages#contact'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
